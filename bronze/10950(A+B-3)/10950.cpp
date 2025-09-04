b43d542a7224065456e23a6c73b1cd4736e2e249
김남호 <159428273+southern03@users.noreply.github.com>
2025-09-05 01:18:58 +0900

    Create 10950.cpp

---
+++ bronze/10950(A+B-3)/10950.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    int A, B;
+    cin >> n;
+    for (int i=0;i<n;i++){
+        cin >> A >> B;
+        cout << A+B << endl;
+    }
+    return 0;
+}
