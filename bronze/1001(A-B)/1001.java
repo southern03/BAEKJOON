64dc3784f63bc432219a1fb06696b54828eca9b6
김남호 <159428273+southern03@users.noreply.github.com>
2025-09-04 19:25:17 +0900

    Create 1001.java

---
+++ bronze/1001(A-B)/1001.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        int A, B;
+        Scanner in = new Scanner(System.in);
+        A=in.nextInt();
+        B=in.nextInt();
+        System.out.println(A-B);
+    }
+}
